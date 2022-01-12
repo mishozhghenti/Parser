3e1c0edf004e0e5b0ac07d8c3d2867120c8632fb
misho <mishozhghenti@gmail.com>
2022-01-12 17:59:49 +0400

    added exceptions

--- src/com/company/core/Parser.java
+++ src/com/company/core/Parser.java
@@ -2,6 +2,7 @@ package com.company.core;
 
 import com.company.model.Item;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -9,18 +10,24 @@ import java.util.List;
 public class Parser {
 
     public static List<Item> parse(String text) {
-        ArrayList<Item> list = new ArrayList<>();
+        if(text == null){
+            throw new InvalidParameterException("text should not be NULL");
+        }
+        List list = new ArrayList<>();
         parseRec(text, 0, list);
         return list;
     }
 
     public static List<Item> sortedParse(String text) {
+        if(text == null){
+            throw new InvalidParameterException("text should not be NULL");
+        }
         List<Item> list = parse(text);
         sort(list);
         return list;
     }
 
-    private static int parseRec(String text, int currentIndex, ArrayList<Item> list) {
+    private static int parseRec(String text, int currentIndex, List list) {
         if (currentIndex >= text.length()) {
             return text.length();
         }
--- src/com/company/utils/Utils.java
+++ src/com/company/utils/Utils.java
@@ -2,17 +2,17 @@ package com.company.utils;
 
 import com.company.model.Item;
 
+import java.security.InvalidParameterException;
 import java.util.Collections;
 import java.util.List;
 
 public class Utils {
 
     public static void printItemsHierarchy(List<Item> list) {
-        if (list != null || list.size() != 0) {
-            printRec(list, 1);
-        } else {
-            System.out.println("### List is empty ###");
+        if (list == null) {
+            throw new InvalidParameterException("list should not be NULL");
         }
+        printRec(list, 1);
     }
 
     private static void printRec(List<Item> list, int depth) {
